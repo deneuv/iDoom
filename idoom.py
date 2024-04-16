263cc496101a08ad25df7ea8db33ca105d82ef56
deneuv <166051402+deneuv@users.noreply.github.com>
2024-04-16 12:38:27 -0400

    New update iDOOM
    now we have a filter to pdf files!

--- idoom.py
+++ idoom.py
@@ -1,59 +1,67 @@
-from termcolor import colored
 import time
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+# Função para verificar se o conteúdo da resposta se parece com um arquivo PDF
+def is_pdf(response):
+    # Verifica se o conteúdo da resposta parece ser um PDF
+    return response.headers.get('content-type') == 'application/pdf'
+
 print("\n")
-print(colored(" ... . .. ... . .. ... .. .. ... . .. . . .... ... ... ... ... .. . . .. .. .. .\n\n", "blue"))
-print(colored(" ██╗██████╗  ██████╗  ██████╗ ███╗   ███╗", "green") + colored("", "red"))
-print(colored(" ██║██╔══██╗██╔═══██╗██╔═══██╗████╗ ████║", "green") + colored("  Creator | ", "white") + colored("Deneuv", "red"))
-print(colored(" ██║██║  ██║██║   ██║██║   ██║██╔████╔██║", "cyan") + colored("  Version | ", "white") + colored("iDooM v beta", "green"))
-print(colored(" ██║██║  ██║██║   ██║██║   ██║██║╚██╔╝██║", "cyan") + colored("  Github  | ", "white") + colored("https://github.com/deneuv/iDoom", "green"))
-print(colored(" ██║██████╔╝╚██████╔╝╚██████╔╝██║ ╚═╝ ██║", "blue") + colored("  Help    | ", "white") + colored("readme.txt", "yellow"))
-print(colored(" ╚═╝╚═════╝  ╚═════╝  ╚═════╝ ╚═╝     ╚═╝", "blue") + colored("", "red"))
-print(colored("\n ... . .. ... . .. ... .. .. ... . .. . . .... ... ... ... ... .. . . .. .. .. .\n", "blue"))
-proton_mail = (colored("deneuv888399281a1871920312@proton.me", "red"))
-info_message = (colored(f"( This is the beta version of iDOOM If you get any \nBug, contact us on {proton_mail} )", "white"))
-print(colored(" . ... .... ... .. .. ..  m a d e   i n    c y b 3 r i a .. .. . . .... . .... ..\n", "magenta"))
-print(colored(f"[Info] {info_message}", "yellow"))
+print(" ... . .. ... . .. ... .. .. ... . .. . . .... ... ... ... ... .. . . .. .. .. .\n\n")
+print(" ██╗██████╗  ██████╗  ██████╗ ███╗   ███╗")
+print(" ██║██╔══██╗██╔═══██╗██╔═══██╗████╗ ████║  Creator | Deneuv")
+print(" ██║██║  ██║██║   ██║██║   ██║██╔████╔██║  Version | iDooM v beta")
+print(" ██║██║  ██║██║   ██║██║   ██║██║╚██╔╝██║  Github  | https://github.com/deneuv/iDoom")
+print(" ██║██████╔╝╚██████╔╝╚██████╔╝██║ ╚═╝ ██║  Help    | readme.txt")
+print(" ╚═╝╚═════╝  ╚═════╝  ╚═════╝ ╚═╝     ╚═╝")
+print("\n ... . .. ... . .. ... .. .. ... . .. . . .... ... ... ... ... .. . . .. .. .. .\n")
+proton_mail = "deneuv888399281a1871920312@proton.me"
+info_message = "( This is the beta version of iDOOM If you get any \nBug, contact us on {proton_mail} )"
+print(" . ... .... ... .. .. ..  m a d e   i n    c y b 3 r i a .. .. . . .... . .... ..\n")
+print(f"[Info] {info_message}")
 
 print(" ")
-target = str(input(colored("[~]", "blue") + colored(" Target URL:", "white")))
-param = str(input(colored("[~]", "blue") + colored(" Parameter:", "white")))
-print(colored("query saved.", "green"))
+target = str(input("[~] Target URL:"))
+param = str(input("[~] Parameter:"))
+print("query saved.")
 try:
-    interval1 = int(input(colored("[~]", "blue") + colored(" Interval between:", "white")))
-    interval2 = int(input(colored("[~]", "blue") + colored(" And:", "white")))
+    interval1 = int(input("[~] Interval between:"))
+    interval2 = int(input("[~] And:"))
 except Exception:
-    print(colored("please, use a int number", "red"))
+    print("please, use a int number")
     exit()
 now = datetime.now()
 time1 = now.strftime("%H:%M:%S")
 print(f"[ running IDoom | https://github.com/deneuv/iDoom | at {time1} ]")
-print(colored("""
+print("""
  __________________________________________________________________________________________
 |                                        RESULTS                                           |
 |__________________________________________________________________________________________|
-""", "blue"))
+""")
 
 try:
     for line in range(interval1, interval2):
         url = f'{target}{param}{line}'
-        connection = requests.get(url)
-        if connection.status_code == 200:
-            soup = BeautifulSoup(connection.text, 'html.parser')
-            if soup.body is not None:
-                body_content = soup.body.get_text()
-                if len(body_content) > 1000:
-                    print(url, ' +++++')
-                else:
-                    print(url, ' -----')
+        response = requests.get(url)
+        sizing = len(response.content)
+        if response.status_code == 200:
+            if is_pdf(response) and sizing > 5000:
+                print(url, ' PDF found')
+            else:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                if soup.body is not None:
+                    body_content = soup.body.get_text()
+                    if len(body_content) > 1000:
+                        print(url, ' +++++')
+                    else:
+                        print(url, ' -----')
         else:
             print("error")
 except Exception:
-    print(colored("something wrong...", "yellow"))
-    print(colored("try again.", "yellow"))
+    print("something wrong...")
+    print("try again.")
     
 now = datetime.now()
 time2 = now.strftime("%H:%M:%S")
